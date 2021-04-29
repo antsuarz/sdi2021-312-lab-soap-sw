0974eb2eddfd951c809313a7936c041071506e72
antsuarz <78346323+antsuarz@users.noreply.github.com>
2021-04-29 20:59:47 +0200

    SDI-2021-312-SW-11.1-Servicio SOAP

---
+++ sdi2021-312-lab-soap-sw/src/main/java/com/uniovi/Sdi2021312LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2021312LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2021312LabSoapSwApplication.class, args);
+	}
+
+}
